f49b8832060f87668fcf4086d15af397f5740552
costica.puntaru <costica.puntaru@everymatrix.com>
2021-12-20 18:01:51 +0200

    added basic react router example

---
+++ src/pages/contact-us.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function ContactUs() {
+
+    return (
+        <div>
+            this is the contact us page
+        </div>
+    )
+}
---
+++ src/pages/not-found.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function NotFound() {
+
+    return (
+        <div>
+            <h1>Eroare 404, Pagina nu exista</h1>
+        </div>
+    )
+}
